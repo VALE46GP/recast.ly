91bf9c8800847b937b2ba61e68d87d817ad1aec1
VALE46GP <ericdoescode@gmail.com>
2018-10-02 11:44:20 -0700

    Search instantiated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em></h5></div>
+        <div><h5><em><Search /> </em></h5></div>
       </div>
     </nav>
     <div className="row">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,25 @@
 var Search = () => (
   <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
+    <input className="form-control" type="text"/>
+    <button className="btn hidden-sm-down" onClick={Search.results}>
       <span className="glyphicon glyphicon-search"></span>
     </button>
-  </div> 
+  </div>
 );
+
+// Search.searchResults = function(searchEntry) {
+//   console.log(searchEntry);
+// };
+
+Search.results = function() {
+  //document.getElementByClassName("btn hidden-sm-down").onclick = function() {
+  console.log($('.form-control').val());
+};
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default Search;
+
+
+//$('.form-control').val()
